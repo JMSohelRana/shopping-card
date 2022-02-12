e256d081c13efb9e2d23b3cd5a7659098eecee89
Sohel <jmsohelrana04@gmail.com>
2022-02-13 00:48:37 +0600

    iphone part added with calculation

--- app.js
+++ app.js
@@ -1,24 +1,34 @@
-function updateCaseNumber(isIncresing) {
-  let caseInput = document.getElementById("case-number");
+function updateProductNumber(product, price, isIncresing) {
+  let productInput = document.getElementById(product + "-number");
 
-  let caseNumber = caseInput.value;
+  let productNumber = productInput.value;
 
   if (isIncresing == true) {
-    caseNumber = parseInt(caseNumber) + 1;
-  } else if (caseNumber > 0) {
-    caseNumber = parseInt(caseNumber) - 1;
+    productNumber = parseInt(productNumber) + 1;
+  } else if (productNumber > 0) {
+    productNumber = parseInt(productNumber) - 1;
   }
-  caseInput.value = caseNumber;
+  productInput.value = productNumber;
 
   //update baalance
-  let caseTotal = document.getElementById("case-total");
-  caseTotal.innerText = caseNumber * 59;
+  let productTotal = document.getElementById(product + "-total");
+  productTotal.innerText = productNumber * price;
 }
 
+//phone increase decrease events
+document.getElementById("phone-plus").addEventListener("click", function () {
+  updateProductNumber("phone", 1200, true);
+});
+
+document.getElementById("phone-minus").addEventListener("click", function () {
+  updateProductNumber("phone", 1200, false);
+});
+
+//handle casing increase decrease events
 document.getElementById("case-plus").addEventListener("click", function () {
-  updateCaseNumber(true);
+  updateProductNumber("case", 60, true);
 });
 
 document.getElementById("case-minus").addEventListener("click", function () {
-  updateCaseNumber(false);
+  updateProductNumber("case", 60, false);
 });
